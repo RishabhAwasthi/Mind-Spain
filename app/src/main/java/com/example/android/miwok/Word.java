d2dc4c0d0beb8d7291500ebee3243caf325976cc
rishabh <rishabh7awashi@gmail.com>
2017-09-29 21:28:29 +0530

    tab version

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,44 @@
+package com.example.android.miwok;
+
+/**
+ * Created by awasthi's on 9/27/2017.
+ */
+
+public class Word {
+    private String eng,spanish;
+   private int imageId=noimage;
+    private static final int noimage=-1;
+    private int mAudioResourceId;
+
+    public Word(String eng, String spanish, int image,int mAudioResourceId) {
+        this.eng = eng;
+        this.spanish = spanish;
+        this.imageId = image;
+        this.mAudioResourceId=mAudioResourceId;
+    }
+
+    public Word(String eng, String spanish,int mAudioResourceId) {
+        this.eng = eng;
+        this.spanish = spanish;
+        this.mAudioResourceId=mAudioResourceId;
+    }
+    public boolean hasimage(){
+        return imageId!=noimage;
+    }
+
+    public String getEng() {
+        return eng;
+    }
+
+    public String getSpanish() {
+        return spanish;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+}
